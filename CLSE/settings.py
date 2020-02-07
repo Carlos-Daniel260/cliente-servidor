b26d9798b8a621f5f826495c6db39e563627816a
Carlos Daniel <183403@ids.upchiapas.edu.mx>
2020-02-07 09:30:18 -0600

    login

--- CLSE/settings.py
+++ CLSE/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'Login',
 ]
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
