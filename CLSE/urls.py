af325a0f0fc3bd204e12b6b2e1b790aeed864d38
Carlos Daniel <183403@ids.upchiapas.edu.mx>
2020-02-11 20:16:05 -0600

    get and set

--- CLSE/urls.py
+++ CLSE/urls.py
@@ -20,20 +20,24 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^',include(router.urls)),
-    re_path(r'^api/v1/login', include('Login.urls'))
+    re_path(r'^', include(router.urls)),
+    re_path(r'^api/v1/', include('Login.urls'))
 ]
