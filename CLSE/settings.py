eaa462b81bfa0a791ec16386fdc4a6c2e61a2295
Carlos Daniel <183403@ids.upchiapas.edu.mx>
2020-02-13 21:02:49 -0600

    bd get and post

--- CLSE/settings.py
+++ CLSE/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'Login',
+    'Profile',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
--- CLSE/urls.py
+++ CLSE/urls.py
@@ -39,5 +39,6 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include(router.urls)),
-    re_path(r'^api/v1/', include('Login.urls'))
+    re_path(r'^api/v1/', include('Login.urls')),
+    re_path(r'^api/v2/', include('Profile.urls'))
 ]
